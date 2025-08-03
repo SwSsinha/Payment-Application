33cba4546b89354bb3e6eaa7af8b0441118a5348
Swastik Sinha <sws23sinha4217@gmail.com>
2025-08-03 19:54:41 +0000

    checking again

--- frontend/src/pages/SendMoney.jsx
+++ frontend/src/pages/SendMoney.jsx
@@ -63,7 +63,7 @@ export const SendMoney = () => {
                             </div>
                             <button onClick={handleTransfer}
                                 className="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white hover:bg-green-600">
-                                Initiate Transfer
+                                Send!
                             </button>
 
 
